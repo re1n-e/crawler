2a786c3c4f186e66196927728ff767914e1809a3
re1n-e <raghavendrabargali@gmail.com>
2025-11-06 23:00:39 +0530

    Updated main.go

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	args := os.Args
 
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	base_url := args[1]
+	fmt.Printf("starting crawl of: %s\n", base_url)
+}

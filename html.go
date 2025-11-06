6b51a7660863207131584aa5c8d66051ef7c0d0b
re1n-e <raghavendrabargali@gmail.com>
2025-11-06 23:00:07 +0530

    Added html.go

---
+++ html.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func getH1FromHTML(html string) (string, error) {
+	// Load the html cotent
+	reader := strings.NewReader(html)
+	docc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return "", fmt.Errorf("-Failed pasring h1 tag: %v", err)
+	}
+
+	var h1Text string
+
+	docc.Find("h1").EachWithBreak(func(_ int, s *goquery.Selection) bool {
+		h1Text = strings.TrimSpace(s.Text())
+		return false
+	})
+
+	return h1Text, nil
+}
+
+func getFirstParagraphFromHTML(html string) (string, error) {
+	reader := strings.NewReader(html)
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return "", fmt.Errorf("failed parsing HTML: %v", err)
+	}
+
+	// First try: <main> p
+	if mainSel := doc.Find("main p").First(); mainSel.Length() > 0 {
+		return strings.TrimSpace(mainSel.Text()), nil
+	}
+
+	// Fallback: first <p>
+	if p := doc.Find("p").First(); p.Length() > 0 {
+		return strings.TrimSpace(p.Text()), nil
+	}
+
+	return "", nil
+}
+
+func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	reader := strings.NewReader(htmlBody)
+	doc, err := goquery.NewDocumentFromReader(reader)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse html body: %v", err)
+	}
+
+	var urls []string
+
+	doc.Find("a[href]").Each(func(_ int, s *goquery.Selection) {
+		href, ok := s.Attr("href")
+		if !ok {
+			return
+		}
+
+		href = strings.TrimSpace(href)
+		if href == "" {
+			return
+		}
+
+		// Parse the URL
+		u, err := url.Parse(href)
+		if err != nil {
+			return
+		}
+
+		// Convert relative → absolute
+		u = baseURL.ResolveReference(u)
+
+		urls = append(urls, u.String())
+	})
+
+	return urls, nil
+}
+
+func getImagesFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	reader := strings.NewReader(htmlBody)
+	doc, err := goquery.NewDocumentFromReader(reader)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse html body: %v", err)
+	}
+
+	var urls []string
+
+	doc.Find("img[src]").Each(func(_ int, s *goquery.Selection) {
+		src, ok := s.Attr("src")
+		if !ok {
+			return
+		}
+
+		if src == "" {
+			return
+		}
+
+		//Parse url
+		u, err := url.Parse(src)
+		if err != nil {
+			return
+		}
+
+		// Convert realtive to absolute
+		u = baseURL.ResolveReference(u)
+		urls = append(urls, u.String())
+	})
+
+	return urls, nil
+}
---
+++ html_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+func TestGetH1FromHTMLBasic(t *testing.T) {
+	inputBody := "<html><body><h1>Test Title</h1></body></html>"
+	actual, err := getH1FromHTML(inputBody)
+	if err != nil {
+		t.Errorf("Test err: %v", err)
+		return
+	}
+	expected := "Test Title"
+
+	if actual != expected {
+		t.Errorf("expected %q, got %q", expected, actual)
+	}
+}
+
+func TestGetFirstParagraphFromHTMLMainPriority(t *testing.T) {
+	inputBody := `<html><body>
+		<p>Outside paragraph.</p>
+		<main>
+			<p>Main paragraph.</p>
+		</main>
+	</body></html>`
+	actual, err := getFirstParagraphFromHTML(inputBody)
+	if err != nil {
+		t.Errorf("Test err: %v", err)
+		return
+	}
+	expected := "Main paragraph."
+
+	if actual != expected {
+		t.Errorf("expected %q, got %q", expected, actual)
+	}
+}
+
+func TestGetURLsFromHTMLAbsolute(t *testing.T) {
+	inputURL := "https://blog.boot.dev"
+	inputBody := `<html><body><a href="https://blog.boot.dev"><span>Boot.dev</span></a></body></html>`
+
+	baseURL, err := url.Parse(inputURL)
+	if err != nil {
+		t.Errorf("couldn't parse input URL: %v", err)
+		return
+	}
+
+	actual, err := getURLsFromHTML(inputBody, baseURL)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expected := []string{"https://blog.boot.dev"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestGetImagesFromHTMLRelative(t *testing.T) {
+	inputURL := "https://blog.boot.dev"
+	inputBody := `<html><body><img src="/logo.png" alt="Logo"></body></html>`
+
+	baseURL, err := url.Parse(inputURL)
+	if err != nil {
+		t.Errorf("couldn't parse input URL: %v", err)
+		return
+	}
+
+	actual, err := getImagesFromHTML(inputBody, baseURL)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expected := []string{"https://blog.boot.dev/logo.png"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
